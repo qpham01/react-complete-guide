7ff93220051828a2338dc18922a22d62651ba61a
= <quoc_pham@msn.com>
2019-01-06 15:59:45 -0600

    Made class wrapper function returns a component class that can have states and lifecycle methods.

--- src/util/wrapClass.js
+++ src/util/wrapClass.js
@@ -1,11 +1,23 @@
 import React from "react";
 
+// const wrapClass = (WrappedComponent, className) => {
+//   return props => (
+//     <div className={className}>
+//       <WrappedComponent {...props}/>
+//     </div>
+//   );
+// };
+
 const wrapClass = (WrappedComponent, className) => {
-  return props => (
-    <div className={className}>
-      <WrappedComponent {...props} />
-    </div>
-  );
+  return class extends React.Component {
+    render() {
+      return (
+        <div className={className}>
+          <WrappedComponent {...this.props} />
+        </div>
+      );
+    }
+  };
 };
 
 export default wrapClass;
